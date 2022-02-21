c62074b8f152fdc839261a3bcab4f730b71949b7
[Jaiendiran M] <[jaiendiran2494@gmail.com]>
2022-02-21 11:56:50 +0530

    changed button text.

--- src/components/ViewExpensesModal.js
+++ src/components/ViewExpensesModal.js
@@ -33,7 +33,7 @@ export default function ViewExpensesModal({budgetId, handleClose}) {
                   <Stack direction='horizontal' gap={2} key={expense.id}>
                     <div className='me-auto fs-4'>{expense.description}</div>
                     <div className='fs-5'>{currencyFormatter.format(expense.amount)}</div>
-                    <Button onClick={() => deleteExpense(expense)} size='sm' variant='outline-danger'>&time;</Button>
+                    <Button onClick={() => deleteExpense(expense)} size='sm' variant='outline-danger'>Remove</Button>
                   </Stack>
                 ))}
               </Stack>
